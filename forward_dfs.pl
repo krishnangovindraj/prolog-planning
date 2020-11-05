59b5c4efe1b5b2e4d6373b8b58662be675110e81
Krishnan Govindraj <krishnangovindraj1794@gmail.com>
2020-11-06 00:55:59 +0100

    clean up forward_dfs Again

--- forward_dfs.pl
+++ forward_dfs.pl
@@ -8,27 +8,35 @@
 search_forward_dfs(StartState, InterestPredicate, MaxDepth, Goals):-
     forward_dfs([], StartState, InterestPredicate, Goals, MaxDepth, []).
 
-explore_node(NextAction, ActionPath, State, InterestPredicate, GoalsReached, MaxDepth, LoopDetector):-
+% ActionPath does not includes Action.
+explore_branch(NextAction, ActionPath, State, InterestPredicate, GoalsReached, MaxDepth, LoopDetector):-
     state_apply_action(State, NextAction, ResultState), % In case it fails
-    forward_dfs([NextAction|ActionPath], ResultState, InterestPredicate, GoalsReached, MaxDepth,  LoopDetector).
+    ResultActionPath = [NextAction|ActionPath],
+
+    not(state_check_loops(ResultState, LoopDetector, ResultActionPath)),
+    (call(InterestPredicate, ResultState, ResultActionPath) -> 
+        (GoalsReached = [ResultActionPath|DownbranchGoals]) ; 
+        (GoalsReached = DownbranchGoals)
+    ),
+    forward_dfs(ResultActionPath, ResultState, InterestPredicate, DownbranchGoals, MaxDepth,  LoopDetector).
 
 % forward_dfs(+ActionPath, +State, +InterestPredicate, -Goals, +MaxDepth_, +LoopDetector)
-forward_dfs(ActionPath, State, _InterestPredicate, [], _, LoopDetector):-
-    state_check_loops(State, LoopDetector, ActionPath), !.
+forward_dfs(_ActionPath, _State, _InterestPredicate, [], MaxDepth, _LoopDetector):-
+    MaxDepth =:= 0, !. % Efficiency cut
 
 forward_dfs(ActionPath, State, InterestPredicate, Goals, MaxDepth, LoopDetector):-
-    not(state_check_loops(State, LoopDetector, ActionPath)), % This is for the cut to not change behaviour. It can be commented out.
-    (call(InterestPredicate, State, ActionPath) -> 
-        (GoalsIn = [ActionPath]) ; 
-        (GoalsIn = [])
+     % This is for the cut to not change behaviour. It can be commented out.
+    MaxDepth =\= 0,  
+    MaxDepth1 is MaxDepth - 1,
+    expand(ActionPath, State, NextLevel),
+    state_update_loopdetector(State, ActionPath, LoopDetector, NextLoopDetector),
+    findall(Goals, 
+        (member(NextAction, NextLevel), explore_branch(NextAction, ActionPath, State, InterestPredicate, Goals, MaxDepth1, NextLoopDetector)),
+        UnflatGoals
     ),
-    (MaxDepth is 0 -> (Goals = GoalsIn) ; ( 
-        MaxDepth1 is MaxDepth - 1,
-        expand(ActionPath, State, NextLevel),
-        state_update_loopdetector(State, ActionPath, LoopDetector, NextLoopDetector),
-        findall(Goals, 
-            (member(NextAction, NextLevel), explore_node(NextAction, ActionPath, State, InterestPredicate, Goals, MaxDepth1, NextLoopDetector)),
-            UnflatGoals
-        ),
-        flatten(UnflatGoals, Goals)
-    )).
+    combine_lists(UnflatGoals, Goals).
+
+combine_lists([], []):- !.
+combine_lists([H|T], F):-
+    combine_lists(T, FT),
+    append(H, FT, F).
