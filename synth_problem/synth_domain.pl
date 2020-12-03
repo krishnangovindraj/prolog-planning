a54322e28116cadf10dcf639d9a8f24fbcd0c28c
Krishnan Govindraj <krishnangovindraj1794@gmail.com>
2020-12-03 21:04:58 +0100

    stash

--- synth_problem/synth_domain.pl
+++ synth_problem/synth_domain.pl
@@ -1,25 +1,31 @@
 :- consult(synth_evaluate).
 :- consult(synth_simulate).
-% From the pddl: 
-%%  (:types table sheet model - object
-%%          countor-constraint tacle-constraint segmentation dreaml mercs admercs lgg-rule - model)
+
 
 action(
     load_spreadsheet(S),
-    [data_source_path(S, P), evaluate(synth_load_spreadsheet(P, S))], % I can add something 
+    [data_source_path(S, P),not(spreadsheet(S)), evaluate(synth_load_spreadsheet(P, S))], % You don't want it loaded twice.
     [],
     [spreadsheet(S)]
     ).
 
+% action(
+%     detect_tables(S,T), 
+%     [spreadsheet(S), evaluate(synth_detect_table(S,T))], % not(table(S,T)) can be used if the tables are all added at once.
+%     [],
+%     [table(S, T)]
+% ).
+
+
 action(
-    detect_tables(S,T), 
-    [spreadsheet(S), evaluate(synth_detect_table(S,T))],
+    detect_all_tables(S,TableList), 
+    [spreadsheet(S), not(all_tables_detected(S)), evaluate(synth_detect_table(S,TableList))], 
     [],
-    [table(S, T)]
+    [all_tables_detected(S)| TableList]
 ).
 
 action(
-    get_field_types(T, FT),
+    get_field_types(T, FieldTypeList),
     [ table(S,T),   ],
     [ ],
     [ field_type(FT) ] % This might get confusing since FT is a compound field_type(X,Y,Z)
