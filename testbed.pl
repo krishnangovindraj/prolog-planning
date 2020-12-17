62d0941861fc23e04e5561a19c56777e50d9a23c
Krishnan Govindraj <krishnangovindraj1794@gmail.com>
2020-12-17 18:02:00 +0100

    Add some hax to make interactive more usable

--- testbed.pl
+++ testbed.pl
@@ -9,3 +9,5 @@
 %   ?- initial_state(SL), state_create(SL, S),!, search_forward_dfs(S, goal_check, 4, G). % As many as you want.
 %   
 % You can use a different MaxDepth. If you have loop detection on, you can set MaxDepth to -1 (no depth limit).
+
+:- consult('kg_hax.pl').
